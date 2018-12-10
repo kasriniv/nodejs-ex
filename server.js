c783f32b2e866e3b3802aafc013195d6b32d8408
Kavitha Srinivasan <kasriniv@users.noreply.github.com>
2018-12-10 17:59:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-app.use(bodyParser.text({ type: 'text/plain' }));
+app.use(bodyParser.text({ type: 'text/*' }));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
